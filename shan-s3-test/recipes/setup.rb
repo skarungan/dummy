473739f74fe215d65b3f2bd023f3158488101ac1
Shanmuga Sundaram K <shanmugakarna89@gmail.com>
2017-06-23 22:52:43 +0530

    Update setup.rb

--- shan-s3-test/recipes/setup.rb
+++ shan-s3-test/recipes/setup.rb
@@ -4,13 +4,4 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'java'
-include_recipe 'apt'
-
-s3_file "/usr/local/bin/terraform" do
-    remote_path "terraform_0.9.8_darwin_amd64.zip"
-    bucket "opsworks-test-s3-file"
-    s3_url "https://s3.amazonaws.com/opsworks-test-s3-file"
-    mode "0755"
-    action :create
-end
+Chef::Log.error(node['memory']['total'] / 1024)
