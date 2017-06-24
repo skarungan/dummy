8bbe1b96f0311b198fc6a4ed9f1e61e1497f359c
Shanmuga Sundaram K <shanmugakarna89@gmail.com>
2017-06-24 08:23:21 +0530

    Update setup.rb

--- shan-s3-test/recipes/setup.rb
+++ shan-s3-test/recipes/setup.rb
@@ -4,4 +4,17 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-Chef::Log.error(node['memory']['total'] / 1024)
+memory_in_megabytes = case node['os']
+when /.*bsd/
+  node.memory.total.to_i / 1024 / 1024
+when 'linux'
+  node.memory.total[/\d*/].to_i / 1024
+when 'darwin'
+  node.memory.total[/\d*/].to_i
+when 'windows', 'solaris', 'hpux', 'aix'
+  node.memory.total[/\d*/].to_i / 1024
+end
+
+Chef::Log.error(memory_in_megabytes)
+Chef::Log.error(node['cpu']['total'])
+
